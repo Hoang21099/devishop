f4b2836918cbe4ef462a3247fcead5fc8f1addc2
devichhoang <hoang21099@gmail.com>
2019-10-05 19:50:25 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//Component
+import HomePage from './components/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
