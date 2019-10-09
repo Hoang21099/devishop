bdd620344613259a63d811466a9b1d4c5bab412c
devichhoang <hoang21099@gmail.com>
2019-10-09 10:42:45 +0700

    Adding header component and links to home

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -6,7 +6,9 @@ const CollectionItem = ({name,price,imageUrl}) =>(
     <div className="collection-item">
         <div className="image" 
             style={{backgroundImage:`url(${imageUrl})`}}
-        />
+        >
+            <div className="add-cart"><span>Add to Cart</span></div>
+        </div>
         <div className="collection-footer">
             <div className="name">{name}</div>
             <div className="price">${price}</div>
